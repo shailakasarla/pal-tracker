9557518df705f55719da0aacef29f57edec8016d
shaila kasarla <shailakasarla@gmail.com>
2018-05-07 13:44:16 -0600

    made h lower case

--- src/main/java/io/pivotal/pal/tracker/WelcomeController.java
+++ src/main/java/io/pivotal/pal/tracker/WelcomeController.java
@@ -1,13 +1,23 @@
 package io.pivotal.pal.tracker;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class WelcomeController {
+
+    private String welcomeMessage;
+
+    public WelcomeController(
+            @Value("${WELCOME_MESSAGE:NOT SET}") String welcomeMessage)
+        {
+            this.welcomeMessage = welcomeMessage;
+        }
+
     @GetMapping("/")
-    public String SayHello(){
-        return "hello";
+    public String sayHello(){
+        return this.welcomeMessage;
 
     }
 
