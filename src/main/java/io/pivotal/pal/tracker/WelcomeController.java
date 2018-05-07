64ef2aee59533d079bd047772bccdf0bddb3d2e0
shaila kasarla <shailakasarla@gmail.com>
2018-05-07 11:33:28 -0600

    made h lower case

--- src/main/java/io/pivotal/pal/tracker/WelcomeController.java
+++ src/main/java/io/pivotal/pal/tracker/WelcomeController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class WelcomeController {
     @GetMapping("/")
     public String SayHello(){
-        return "Hello";
+        return "hello";
 
     }
 
